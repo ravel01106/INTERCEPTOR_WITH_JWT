6e35eaa0f116483333511f67c6bcd6467597ac1e
ravel01106 <amandaravelocabrera@gmail.com>
2024-09-24 10:12:31 +0100

    feat: create UserService interface

--- src/main/java/com/interceptor/example/user/domain/models/User.java
+++ src/main/java/com/interceptor/example/user/domain/models/User.java
@@ -1,5 +1,6 @@
 package com.interceptor.example.user.domain.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(name = "username", unique = true)
   private String username;
 
   private String password;
