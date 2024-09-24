6e1e322bfd1e73fa1bbeb56e84df0bd907e1c0c9
ravel01106 <amandaravelocabrera@gmail.com>
2024-09-24 10:18:56 +0100

    feat: added mysql database configuration

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into users(username, password) values ("admin", "admin")
+insert into users_info(username, name, lastname, email) values("admin", "administrator", "administrator", "administrator@gmail.com")
