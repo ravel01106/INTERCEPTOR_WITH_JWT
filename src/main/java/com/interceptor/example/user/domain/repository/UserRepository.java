ff7c5b3311c0b541a34bef4e45dbaad147f759ce
ravel01106 <amandaravelocabrera@gmail.com>
2024-09-24 11:48:10 +0100

    feat: created User repository

---
+++ src/main/java/com/interceptor/example/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.interceptor.example.user.domain.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.interceptor.example.user.domain.models.User;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+}
