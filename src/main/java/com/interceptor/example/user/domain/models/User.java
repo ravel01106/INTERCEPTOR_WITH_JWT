a3de606a2263b9589f4700472a234fe8d07c216b
ravel01106 <amandaravelocabrera@gmail.com>
2024-09-24 12:58:45 +0100

    feat: created the one to one relation ship with user and userinfo entities

--- src/main/java/com/interceptor/example/user/domain/models/User.java
+++ src/main/java/com/interceptor/example/user/domain/models/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,12 +15,17 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
   private Long id;
 
   @Column(name = "username", unique = true)
   private String username;
 
   private String password;
+
+  @OneToOne
+  @JoinColumn(name = "username", referencedColumnName = "username")
+  private UserInfo userInfo;
 
   public User(Long id, String username, String password) {
     this.id = id;
@@ -57,5 +64,20 @@ public class User {
   public void setPassword(String password) {
     this.password = password;
   }
+
+  public UserInfo getUserInfo() {
+    return userInfo;
+  }
+
+  public void setUserInfo(UserInfo userInfo) {
+    this.userInfo = userInfo;
+  }
+
+  @Override
+  public String toString() {
+    return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+  }
+
+
 
 }
--- src/main/java/com/interceptor/example/user/domain/models/UserInfo.java
+++ src/main/java/com/interceptor/example/user/domain/models/UserInfo.java
@@ -1,10 +1,12 @@
 package com.interceptor.example.user.domain.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,10 +15,11 @@ public class UserInfo {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "userinfo_id")
   private Long id;
 
-  @Column(name = "username", unique = true)
-  private String username;
+  @OneToOne(mappedBy = "userInfo", cascade = CascadeType.ALL, orphanRemoval = true)
+  private User user;
 
   private String name;
 
@@ -27,8 +30,8 @@ public class UserInfo {
   public UserInfo() {
   }
 
-  public UserInfo(String username, String name, String lastname, String email) {
-    this.username = username;
+  public UserInfo(User user, String name, String lastname, String email) {
+    this.user = user;
     this.name = name;
     this.lastname = lastname;
     this.email = email;
@@ -42,13 +45,6 @@ public class UserInfo {
     this.id = id;
   }
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
 
   public String getName() {
     return name;
@@ -72,6 +68,20 @@ public class UserInfo {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public String toString() {
+    return "UserInfo [id=" + id + ", user=" + user + ", name=" + name + ", lastname=" + lastname + ", email=" + email
+        + "]";
   }
 
 }
