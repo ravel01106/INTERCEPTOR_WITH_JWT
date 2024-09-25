70d945abdcab7a58b219f21fb9a83d10035477c0
ravel01106 <amandaravelocabrera@gmail.com>
2024-09-25 13:11:26 +0100

    feat: created UserController in infrastructure layer

---
+++ src/main/java/com/interceptor/example/user/domain/errors/UserException.java
@@ -0,0 +1,8 @@
+package com.interceptor.example.user.domain.errors;
+
+public class UserException extends Exception{
+
+  public UserException(String message){
+    super(message);
+  }
+}
---
+++ src/main/java/com/interceptor/example/user/infrastructure/UserController.java
@@ -0,0 +1,63 @@
+package com.interceptor.example.user.infrastructure;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.interceptor.example.user.domain.errors.UserException;
+import com.interceptor.example.user.domain.models.User;
+import com.interceptor.example.user.domain.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+  @Autowired
+  UserService userService;
+
+  @PostMapping("/login")
+  public ResponseEntity<User> login(@RequestBody User user) throws UserException {
+      User userLogin = userService.login(user);
+
+      if (userLogin == null){
+        throw new UserException("User incorrect");
+      }
+
+      return new ResponseEntity<User>(userLogin, HttpStatus.OK);
+  }
+
+  @PostMapping("/logout")
+  public ResponseEntity<String> logout(@RequestBody User user) throws UserException {
+      Boolean isUserLogout = userService.logout(user);
+
+      if (isUserLogout == null){
+        throw new UserException("User incorrect");
+      }
+
+      return new ResponseEntity<String>("The user is logout correctly", HttpStatus.OK);
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<User> register(@RequestBody User user) throws UserException {
+    User userFindByUsername = userService.getUserByUsername(user.getUsername());
+
+    if (userFindByUsername == null) {
+      throw new UserException("The user already exists");
+    }
+
+    User userRegister = userService.register(user);
+
+    if (userRegister == null){
+        throw new UserException("ERROR register user");
+    }
+
+    return new ResponseEntity<User>(userRegister, HttpStatus.OK);
+  }
+
+}
